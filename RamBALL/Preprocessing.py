4b133d109fcf632ff51c1ca52da694ea450f9f9c
Lusheng Li <79950798+LushengLi9909@users.noreply.github.com>
2023-08-15 09:18:30 -0500

    Add files via upload

---
+++ RamBALL/Preprocessing.py
@@ -0,0 +1,30 @@
+import numpy as np
+import pandas as pd
+
+class TPM():
+    def counts2TPM(count, efflength):
+        RPK = count / (efflength / 1000)  # Reads Per Kilobase
+        PMSC_rpk = np.sum(RPK) / 1e6      # "per million" scaling factor
+        return RPK / PMSC_rpk
+    
+    def transform(counts):
+        efflen = pd.read_csv('Data/geneid_efflen_vm25_gencode.csv')
+        efflen.index = efflen['geneid']
+        efflen = efflen.drop(columns='geneid')
+        intset = np.intersect1d(counts.columns, efflen.index)
+        counts = counts.loc[:,intset]
+        rown = counts.index
+        coln = counts.columns
+        efflen1 = efflen.loc[intset,] 
+        tpm = np.apply_along_axis(lambda c: counts2TPM(c ,efflen1['efflen']), 1, counts)
+        tpm = np.log2(tpm +1)
+        tpm = pd.DataFrame(tpm)
+        tpm.index = rown
+        tpm.columns = coln
+        ensembl = pd.read_csv('/Data/ensembl_list.csv')
+        tpm_n = pd.DataFrame(0, index=tpm.index, columns=ensembl['ensembl'])
+        for n in tpm.columns:
+            if n in ensembl['ensembl'].values:
+                tpm_n.loc[:,n] = tpm.loc[:,n]
+
+        return tpm_n
---
+++ RamBALL/train_and_predict.py
@@ -0,0 +1,67 @@
+import numpy as np
+import pandas as pd
+from sklearn.svm import SVC
+from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import accuracy_score, precision_score, f1_score, recall_score, roc_auc_score
+from scipy.sparse import csr_matrix
+import math
+
+def ranM(scdata, p, seedn):
+    # for random projection; note scdata: m*n, m is the feature dimensions, n is the sample number; p is the reduced dimension
+    m = scdata.shape[0]  # the number of features
+    n = scdata.shape[1]  # number of samples/cells
+
+    s = np.sqrt(m)  # according to the paper 'Very Sparse Random Projection'
+
+    if seedn % 1 == 0:
+        np.random.seed(int(seedn))
+        x0 = np.random.choice([np.sqrt(s), 0, -np.sqrt(s)], size=m * p, replace=True, p=[1/(2*s), 1 - 1/s, 1/(2*s)])
+    else:
+        x0 = np.random.choice([np.sqrt(s), 0, -np.sqrt(s)], size=m * p, replace=True, p=[1/(2*s), 1 - 1/s, 1/(2*s)])
+
+    x = csr_matrix(x0.reshape(m, p), dtype=np.float32)
+
+    return x  # the same format, feature*sample
+
+def train_and_predict(tpm_test):
+    subtype = pd.read_csv('/kaggle/input/subtype/subtype.csv',encoding='gbk')
+    dind = subtype[subtype.iloc[:,4] == '0'].index
+    subtype = subtype.drop(index= dind)
+    tpm_train = pd.read_csv('/kaggle/input/ball-tpm/filter_HTSeq_TPM.csv')
+    tpm_train = tpm_train.drop(index= subtype['patient'])
+    ind = tpm_train[tpm_train.subtype == 'Other'].index
+    tpm_train = tpm_train.drop(index= ind)
+    cind = tpm_train[tpm_train.subtype == 'CRLF2(non-Ph-like)'].index
+    tpm_train = tpm_train.drop(index= cind)
+    tpm_train.loc[:,'subtype'][tpm_train.loc[:, 'subtype'] == 'ZNF384'] = 'ZNF384 group'
+    tpm_train.loc[:,'subtype'][tpm_train.loc[:, 'subtype'] == 'ZNF384-like'] = 'ZNF384 group'
+    tpm_train.loc[:,'subtype'][tpm_train.loc[:, 'subtype'] == 'KMT2A'] = 'KMT2A group'
+    tpm_train.loc[:,'subtype'][tpm_train.loc[:, 'subtype'] == 'KMT2A-like'] = 'KMT2A group'
+    enc = LabelEncoder()
+    enc.fit(tpm_train['subtype']) 
+    tpm_train['subtype'] = enc.transform(tpm_train['subtype'])
+    X_traintpm = tpm_train.drop(['subtype'], axis=1)
+    ytpm = tpm_train['subtype']
+    merged_tpm =pd.concat([X_traintpm, tpm_test], ignore_index=False)
+    score = np.zeros(shape = (len(tpm_test.index),20))
+    P = [600,700,800,900,1000,1100,1200,1300,1400,1500,1600,1700,1800,1900,2000,2100,2200,2300,2400,2500]
+    for p in P:
+        oritpm = np.array(merged_tpm.T)
+        rM = ranM(merged_tpm.T, p, 0.5)
+        ntpm = (1 / np.sqrt(p)) * np.transpose(rM) @ oritpm
+        ntpm = np.transpose(ntpm)
+        model = SVC(kernel = 'linear',probability = False)
+        ntpm = pd.DataFrame(ntpm)
+        ntpm.index = merged_tpm.index
+        model.fit(ntpm.loc[X_traintpm.index,], ytpm)
+        score = score + model.decision_function(ntpm.loc[tpm_test.index,])
+    final_score = score / len(P)
+
+    pred = np.argmax(final_score,axis = 1)
+    sub_pred = enc.inverse_transform(pred)
+
+    results = {'Samples':tpm_test.index, 'Subtype_pred': sub_pred}
+    results = pd.DataFrame(results)
+    results.to_csv('Prediction_results.csv', index=False)
+    print(results)
+    return results
